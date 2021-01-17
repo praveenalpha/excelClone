077057058650e164529439f4b224f6bb8eab2ee9
praveenalpha <pm495949@gmail.com>
2021-01-17 12:28:24 +0530

    in fat arrow function this keyword remains same throughout its lifecycle

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const $ = require('jquery');
 
+
+//address added to addressBar
 $(document).on("click",".cell", function () {
+    console.log(this);
     let row = $(this).attr('r-id');
     let col = Number($(this).attr('c-id')) + Number(65);
     let res = String.fromCharCode(col);
-    $('.address').val(res+row);
+    $('.addressBar').val(res+row);
 });
+
+
+//add formula
+$(document).on("change", ".input-formula", function() {
+    let c = $('.input-formula').val();
+    console.log(c);
+})
