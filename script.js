06d04fe9698a1bc8e28b6970b491cd88bfa24b01
praveenalpha <pm495949@gmail.com>
2021-01-18 10:10:45 +0530

    blurr added

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const $ = require('jquery');
 
+//initialise DB
+let db=[];
+let lastClickedCell = [];
+for(let i=0;i<100;i++){
+    let row=[];
+    for(let j=0;j<26;j++){
+        let Name = String.fromCharCode(65+j)+i;
+        let obj = {
+            name: Name,
+            val: "",
+            formula: ""
+        }
+        row.push(obj);
+    }
+    db.push(row);
+}
 
-//address added to addressBar
-$(document).on("click",".cell", function () {
-    console.log(this);
-    let row = $(this).attr('r-id');
-    let col = Number($(this).attr('c-id')) + Number(65);
-    let res = String.fromCharCode(col);
-    $('.addressBar').val(res+row);
+
+$(document).ready(function() {
+    //address bar
+    $('.cell').click(function(){
+        // console.log(db);
+        let row = Number($(this).attr('r-id'));
+        let col = Number($(this).attr('c-id')) + Number(65);
+        let res = String.fromCharCode(col);
+        $('.addressBar').val(res+String(row));
+        col -= 65;
+        lastClickedCell = {row, col};
+    })
+    //input formula
+    $('.input-formula').click(function(){
+        $('.input-formula').blur(function(){
+            let formulaValue = $('.input-formula').val();
+            if(formulaValue){
+                db[lastClickedCell.row][lastClickedCell.col].formula=formulaValue;
+                // console.log(db);
+            }
+        })
+    })
 });
-
-
-//add formula
-$(document).on("change", ".input-formula", function() {
-    let c = $('.input-formula').val();
-    console.log(c);
-})
